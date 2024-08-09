2e90b68c1bef5531925660d6e33c84d0517b4a9e
Ronal Casani <ronalsistemas@gmail.com>
2024-08-09 18:26:06 -0500

    Titulo_Proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Libreria-App';
+  title = 'Bienvenido a la Libreria Ronal';
 }
